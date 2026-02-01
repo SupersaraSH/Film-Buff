9a019835eb084aab39fef0bc1848dcbf73af99b5
Sara Hoces <sarasaezhoces@gmail.com>
2026-02-01 01:03:29 +0100

    Logic completed, index with light and profile almost ready

---
+++ utils/turnOnLight.js
@@ -0,0 +1,7 @@
+const turnOnLight = () => {
+  console.log("holiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii")
+  document.getElementById("section-movies").classList.toggle("light-on");
+
+}
+
+module.exports = turnOnLight;
